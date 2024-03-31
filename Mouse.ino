0893bcadd109ccb6966f6d53be5781b175cfe8f7
Sharp Santillán <adrian.shura18@gmail.com>
2024-03-31 16:48:53 -0500

    fix

--- Mouse.ino
+++ Mouse.ino
@@ -8,6 +8,8 @@ HIDBoot < USB_HID_PROTOCOL_KEYBOARD | USB_HID_PROTOCOL_MOUSE > HidComposite(&Usb
 HIDBoot<USB_HID_PROTOCOL_MOUSE>    HidMouse(&Usb);
 
 int delta[2];
+int negMax = -127;
+int posMax = 127;
 // Mouse
 int lmb = 0;
 int rmb = 0;
@@ -58,7 +60,7 @@ MouseRptParser MousePrs;
 
 void setup()
 {
-  AbsoluteMouse.begin();
+  //AbsoluteMouse.begin();
   Mouse.begin();
   Serial.begin( 115200 );
   Usb.Init();
@@ -71,22 +73,22 @@ void loop()
   delta[0] = 0;
   delta[1] = 0;
   Usb.Task();
-   // Left Mouse
-  if (lmb == 0){
+  // Left Mouse
+  if (lmb == 0) {
     Mouse.release(MOUSE_LEFT);
-  } else if (lmb == 1){
+  } else if (lmb == 1) {
     Mouse.press(MOUSE_LEFT);
   }
   // Right Mouse
-  if (rmb == 0){
+  if (rmb == 0) {
     Mouse.release(MOUSE_RIGHT);
-  } else if (rmb == 1){
+  } else if (rmb == 1) {
     Mouse.press(MOUSE_RIGHT);
   }
   // Middle Mouse
-  if (mmb == 0){
+  if (mmb == 0) {
     Mouse.release(MOUSE_MIDDLE);
-  } else if (mmb == 1){
+  } else if (mmb == 1) {
     Mouse.press(MOUSE_MIDDLE);
   }
 
@@ -101,11 +103,105 @@ void loop()
     Serial.println(data);
 
     // DeltaX & DeltaY
-    delta[0] = data.substring(0, ohHiMarc).toInt();
-    delta[1] = data.substring(ohHiMarc + 1).toInt();
+    int x_value = data.substring(0, ohHiMarc).toInt();
+    delta[0] = map(x_value, 0, 1920, 0, 820);
+    int y_value = data.substring(ohHiMarc + 1).toInt();
+    delta[1] = map(y_value, 0, 1080, 0, 480);
+    handleX(delta[0]);
+    handleY(delta[1]);
     //AbsoluteMouse.moveTo(delta[0], delta[1]);
-    AbsoluteMouse.moveTo(delta[0], delta[1]);
-  } else{
+  } else {
     Mouse.move(delta[0], delta[1]);
   }
 }
+
+// Handle Moving of x 
+void handleX(int dx){
+  
+  int spawns; 
+  int remainder;
+  
+  if(dx < negMax)
+  {
+    // How many times we move mouse
+    spawns = int(dx / negMax); 
+    
+    // How much we move after for loop
+    remainder = int(dx % negMax);
+
+    // Because we can only move 125 at a time,
+    // we need a for loop to spawn multiple mouse events.
+    for(int i = 0; i < spawns; i++)
+    {
+      Mouse.move(negMax , 0, 0);
+    }
+    // Move Remainder
+    Mouse.move(remainder, 0, 0);
+  } 
+  else if (dx >= negMax && dx <= posMax)
+  {
+    Mouse.move(dx, 0, 0);
+  }
+  else if (dx > posMax)
+  {
+    // How many times we move mouse
+    spawns = int(dx / posMax); 
+    
+    // How much we move after for loop
+    remainder = int(dx % posMax);
+    
+    for(int i = 0; i < spawns; i++)
+    {
+      Mouse.move(posMax , 0, 0);
+    }
+    // Move Remainder
+    Mouse.move(remainder, 0, 0);
+  }
+  
+}
+
+// Handle Moving of x 
+void handleY(int dy){
+  
+  int spawns; 
+  int remainder;
+  // MindTrip, Neg is pos & Pos is Neg for move, hence the inverted pos & Neg
+  if(dy < negMax)
+  {
+    // How many times we move mouse
+    spawns = int(dy / negMax); 
+    
+    // How much we move after for loop. -1 converts to correct direction on arduino (pos,neg,neg,pos)
+    remainder = int(dy % negMax);
+    remainder *= -1;
+    // Because we can only move 125 at a time,
+    // we need a for loop to spawn multiple mouse events.
+    for(int i = 0; i < spawns; i++)
+    {
+      Mouse.move(0, posMax, 0);
+    }
+    // Move Remainder
+    Mouse.move(0, remainder, 0);
+  } 
+  else if (dy >= negMax && dy <= posMax)
+  {
+    dy *= -1;
+    Mouse.move(0, dy, 0);
+  }
+  else if (dy > posMax)
+  {
+    // How many times we move mouse
+    spawns = int(dy / posMax); 
+    
+    // How much we move after for loop
+    remainder = int(dy % posMax);
+    remainder *= -1;
+    
+    for(int i = 0; i < spawns; i++)
+    {
+      Mouse.move(0, negMax, 0);
+    }
+    // Move Remainder
+    Mouse.move(0, remainder, 0);
+  }
+};
